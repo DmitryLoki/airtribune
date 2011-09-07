67e134373c4a7b0b4f448099ceb4da208a2a2fdb
Brandonian <brandon@brandonmorrison.com>
2011-09-07 07:12:52 -0400

    Better js support for multiple maps.

--- modules/geofield_map/js/geofield_map.js
+++ modules/geofield_map/js/geofield_map.js
@@ -2,51 +2,62 @@
   Drupal.behaviors.geofieldMap = {
     attach: function(context) {
       var settings = Drupal.settings.geofieldMap;
-      var data = settings.data;
-      var markers = [];
       
-      var latlng = new google.maps.LatLng(41, -80);
       var myOptions = {
         zoom: 8,
-        center: latlng,
         mapTypeId: google.maps.MapTypeId.ROADMAP
       };
-      var map = new google.maps.Map(document.getElementById(settings.map_id),
-          myOptions);
       
-      var range = new google.maps.LatLngBounds();
-      
-      var infowindow = new google.maps.InfoWindow({
-        content: ''
-      });
-      
-      for (var i in data) {
-        var point = new google.maps.LatLng(data[i].lat, data[i].lon);
-        range.extend(point);
-        var marker = new google.maps.Marker({
-          position: point,
-          map: map,
-          title: "test"
-        });
-  
-        if (data[i].icon != undefined) {
-          marker.setIcon(data[i].icon);
-        }
-        marker.setValues({'data_id': i});
-        
-        google.maps.event.addListener(marker, 'click', function() {
-          if (data[this.data_id].text) {
-            infowindow.setContent(data[this.data_id].text);
-            infowindow.open(map, this);
+      $('.geofieldMap:not(.processed)').each(function(index, element) {
+        var data = undefined;
+        for (var i in settings) {
+          if (settings[i].map_id == $(element).attr('id')) {
+            data = settings[i].data;
+            break;
           }
-        });
-      }
+        }
+        
+        if (data != undefined) {
+          var markers = [];
+          
+          var map = new google.maps.Map(document.getElementById($(element).attr('id')), myOptions);
+          
+          
+          var range = new google.maps.LatLngBounds();
       
-      if (i > 0) {
-        map.fitBounds(range);
-      } else {
-        map.setCenter(range.getCenter());
-      }
+          var infowindow = new google.maps.InfoWindow({
+            content: ''
+          });
+          
+          for (var i in data) {
+            var point = new google.maps.LatLng(data[i].lat, data[i].lon);
+            range.extend(point);
+            var marker = new google.maps.Marker({
+              position: point,
+              map: map,
+              title: "test"
+            });
+      
+            if (data[i].icon != undefined) {
+              marker.setIcon(data[i].icon);
+            }
+            marker.setValues({'data_id': i});
+            
+            google.maps.event.addListener(marker, 'click', function() {
+              if (data[this.data_id].text) {
+                infowindow.setContent(data[this.data_id].text);
+                infowindow.open(map, this);
+              }
+            });
+          }
+          
+          if (i > 0) {
+            map.fitBounds(range);
+          } else {
+            map.setCenter(range.getCenter());
+          }
+        }
+      });
     }
-  };
+  }
 })(jQuery);
