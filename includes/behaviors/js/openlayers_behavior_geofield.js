73bc79551a9da9e1f3be0832226f6d7bab592d35
nod_ <nod_@598310.no-reply.drupal.org>
2011-11-03 09:19:31 -0400

    [#1273802] - Openlayers widget refactor

--- includes/behaviors/js/openlayers_behavior_geofield.js
+++ includes/behaviors/js/openlayers_behavior_geofield.js
@@ -2,249 +2,98 @@
  * @file
  * JS Implementation of OpenLayers behavior.
  */
-
-
 (function($) {
+  /**
+   * Geofield Behavior
+   */
+  Drupal.behaviors.openlayers_behavior_geofield = {
+    'attach': function(context, settings) {
+      var data = $(context).data('openlayers'),
+          behavior = data && data.map.behaviors['openlayers_behavior_geofield'],
+          dataProjection = new OpenLayers.Projection('EPSG:4326'),
+          features, wktFormat;
 
-  // Constants
-  var MAP_STATE_PANNING = 'panning';
-  var MAP_STATE_DRAWING = 'drawing';
-  
-  var DRAW_POINT = 'draw point';
-  var DRAW_LINE = 'draw line';
-  var DRAW_POLY = 'draw polygon';
-  var DRAW_BOUNDS = 'draw bounds';
-
-/**
- * Geofield Behavior
- */
-Drupal.behaviors.openlayers_behavior_geofield = {
-  'attach': function(context, settings) {
-    var data = $(context).data('openlayers');
-
-    if (data && data.map.behaviors['openlayers_behavior_geofield']) {
-      data.openlayers.data_form = {
-        'wkt':$(data.map.behaviors['openlayers_behavior_geofield']['wkt']),
-        'type':$(data.map.behaviors['openlayers_behavior_geofield']['type']),
-        'lat':$(data.map.behaviors['openlayers_behavior_geofield']['lat']),
-        'lon':$(data.map.behaviors['openlayers_behavior_geofield']['lon']),
-        'left':$(data.map.behaviors['openlayers_behavior_geofield']['left']),
-        'top':$(data.map.behaviors['openlayers_behavior_geofield']['top']),
-        'right':$(data.map.behaviors['openlayers_behavior_geofield']['right']),
-        'bottom':$(data.map.behaviors['openlayers_behavior_geofield']['bottom'])
-      };
-
-      selection_layer = new OpenLayers.Layer.Vector('Selection Layer');
-      selection_layer.styleMap = Drupal.openlayers.getStyleMap(data.map, 'openlayers_drawfeatures_layer');
-      data.openlayers.addLayer(selection_layer);
-
-      // Controls for Openlayers interaction for points, lines, polygons, and bounds
-      
-      point_control = new OpenLayers.Control.DrawFeature(
-        selection_layer,
-        OpenLayers.Handler.Point,
-        {
-          featureAdded: setItem
-        }
-      );
-      data.openlayers.addControl(point_control);
-
-      line_control = new OpenLayers.Control.DrawFeature(
-        selection_layer,
-        OpenLayers.Handler.Path,
-        {
-          featureAdded: setItem
-        }
-      );
-      data.openlayers.addControl(line_control);
-
-      polygon_control = new OpenLayers.Control.DrawFeature(
-        selection_layer,
-        OpenLayers.Handler.Polygon,
-        {
-          featureAdded: setItem
-        }
-      );
-      data.openlayers.addControl(polygon_control);
-
-      bounds_control = new OpenLayers.Control.DrawFeature(
-        selection_layer,
-        OpenLayers.Handler.RegularPolygon,
-        {
-          featureAdded: setItem
-        }
-      );
-      bounds_control.handler.setOptions({
-          'sides': 4,
-          'irregular': true});
-      data.openlayers.addControl(bounds_control);
-
-      // Add buttons to control_panel for each control type
-      geofield_button = new OpenLayers.Control.Button({
-        displayClass: "openlayers_behavior_geofield_button status", 
-        title: Drupal.t('Geofield Controls')
-      });
-
-      // Create a panel to hold control buttons
-      button_panel = new OpenLayers.Control.Panel({
-        displayClass: 'openlayers_behavior_geofield_button_panel'
-      });
-
-      button_panel.addControls([geofield_button]);
-
-      data.openlayers.addControl(button_panel);
-
-      $('.openlayers_behavior_geofield_button_panel .openlayers_behavior_geofield_button', context).append('<div class=""><ul class="current"><li class="point">Add a point</li></ul><ul class="options"><li class="line">Draw a line</li><li class="polygon">Draw a polygon</li><li class="bounds">Draw bounds</li></ul></div><div class="expand">Expand</div>');
-      $('.openlayers_behavior_geofield_button_panel .openlayers_behavior_geofield_button .options', context).hide();
-      $('.openlayers_behavior_geofield_button_panel .openlayers_behavior_geofield_button .expand', context).click(function() {
-        $('.openlayers_behavior_geofield_button_panel .openlayers_behavior_geofield_button .options', context).slideToggle();
-      });
-
-      $('.openlayers_behavior_geofield_button_panel .openlayers_behavior_geofield_button li', context).click(function() {
-        if (!$(this).hasClass('selected')) {
-          $(this).parent().parent().find('li').removeClass('selected');
-          $(this).addClass('selected');
-
-          if ($(this).hasClass('point')) {
-            buttonToggle('point');
-          } else if ($(this).hasClass('line')) {
-            buttonToggle('line');
-          } else if ($(this).hasClass('polygon')) {
-            buttonToggle('polygon');
-          } else if ($(this).hasClass('bounds')) {
-            buttonToggle('bounds');
-          }
-        } else {
-          $(this).parent().parent().find('li').removeClass('selected');
-          buttonToggle('none');
-        }
-      });
-
-      // Hold down control key for polygons
-      // Hold down alt key for lines
-      // Hold down shift for bounds
-      // Eventually, we should make a really nice editing bar for this
-      $(document).keydown(function(event) {
-        // Shift  -> Boundary
-        if (event.keyCode == 16) {
-          point_control.deactivate();
-          line_control.deactivate();
-          polygon_control.deactivate();
-          bounds_control.activate();
-        }
-        // Control  -> Lines
-        if (event.keyCode == 17) {
-          point_control.deactivate();
-          polygon_control.deactivate();
-          bounds_control.deactivate();
-          line_control.activate();
-        }
-        // Alt  -> Polygons
-        if (event.keyCode == 18) {
-          point_control.deactivate();
-          line_control.deactivate();
-          bounds_control.deactivate();
-          polygon_control.activate();
-        }
-      });
-
-      $(document).keyup(function(event) {
-        // On keyup, go back to points
-        if (event.keyCode == 16 || event.keyCode == 17 || event.keyCode == 18) {
-          bounds_control.deactivate();
-          line_control.deactivate();
-          polygon_control.deactivate();
-          point_control.activate();
-        }
-      });
-      
-      /*
-       * Draw features if the form has values
-       */
-      if (data.openlayers.data_form.wkt.val()) {
-        geometry = new OpenLayers.Geometry.fromWKT(data.openlayers.data_form.wkt.val());
-        geometry.transform(new OpenLayers.Projection('EPSG:4326'), data.openlayers.projection);
-        feature = new OpenLayers.Feature.Vector(geometry);
-        selection_layer.addFeatures([feature]);
+      // helper to create a WKT format object with the right projections
+      function initWktFormat (inp, outp) {
+        var WktWriter = new OpenLayers.Format.WKT();
+        WktWriter.internalProjection = inp;
+        WktWriter.externalProjection = outp || dataProjection;
+        return WktWriter;
       }
-      
+
+      // populate our wkt input field
+      function updateWKTField (features) {
+        var WktWriter = initWktFormat(features.object.map.projection);
+        // limits are to be checked server-side, not here.
+        // for a single shape avoid GEOMETRYCOLLECTION
+        var toSerialize = features.object.features;
+        // don't serialize empty feature
+        if (toSerialize.length) {
+          if (toSerialize.length === 1) { toSerialize = toSerialize[0]; }
+          this.val(WktWriter.write(toSerialize));
+        }
+        // but clear the value
+        else {
+          this.val('');
+        }
+      }
+
+      if (behavior && !$(context).hasClass('geofield-processed')) {
+        // we get the .form-item wrapper which is a slibling of our hidden input
+        var $wkt = $(context).closest('.form-item').parent().find('input.geofield_wkt');
+        var dataLayer = new OpenLayers.Layer.Vector(Drupal.t('Feature Layer'), {
+              projection: dataProjection,
+              drupalID: 'openlayers_behavior_geofield'
+            });
+
+        dataLayer.styleMap = Drupal.openlayers.getStyleMap(data.map, 'openlayers_behavior_geofield');
+        data.openlayers.addLayer(dataLayer);
+
+        if ($wkt.val() != '') {
+          wktFormat = initWktFormat(data.openlayers.projection);
+          features = wktFormat.read($wkt.val());
+          dataLayer.addFeatures(features);
+        }
+
+        // registering events late, because adding data
+        // would result in a reprojection loop
+        dataLayer.events.register('featureadded', $wkt, updateWKTField);
+        dataLayer.events.register('featureremoved', $wkt, updateWKTField);
+        dataLayer.events.register('featuremodified', $wkt, updateWKTField);
+
+        // create toolbar
+        var control = new OpenLayers.Control.EditingToolbar(dataLayer);
+        data.openlayers.addControl(control);
+        control.activate();
+
+        // Add modify feature tool
+        control.addControls(new OpenLayers.Control.ModifyFeature(
+          dataLayer, {
+            displayClass: 'olControlModifyFeature',
+            deleteCodes: [46, 68, 100],
+            handleKeypress: function(evt) {
+              if (this.feature && $.inArray(evt.keyCode, this.deleteCodes) > -1) {
+                // We must unselect the feature before we delete it
+                var feature_to_delete = this.feature;
+                this.selectControl.unselectAll();
+                this.layer.removeFeatures([feature_to_delete]);
+              }
+            }
+          }));
+
+        // on submit recalculate everything to be up to date
+        var formData = {
+          'control': control, 
+          'dataLayer': dataLayer
+        };
+        function handleSubmit (e) {
+          $.map(e.data.control.controls, function(c) { c.deactivate(); });
+          dataLayer.events.triggerEvent('featuremodified');
+        }
+        $(context).parents('form').bind('submit', formData, handleSubmit);
+
+        $(context).addClass('geofield-processed');
+      } // if
     }
-  }
-};
-
-// Function when buttons are clicked
-function buttonToggle(which) {
-  if (which == 'bounds') {
-    point_control.deactivate();
-    line_control.deactivate();
-    polygon_control.deactivate();
-    
-    bounds_control.activate();
-  }
-  else if (which == 'line') {
-    point_control.deactivate();
-    polygon_control.deactivate();
-    bounds_control.deactivate();
-    
-    line_control.activate();
-  }
-  else if (which == 'polygon') {
-    point_control.deactivate();
-    bounds_control.deactivate();
-    line_control.deactivate();
-    
-    polygon_control.activate();
-  }
-  else if (which == 'point') {
-    bounds_control.deactivate();
-    line_control.deactivate();
-    polygon_control.deactivate();
-    
-    point_control.activate();
-  }
-  else {
-    point_control.deactivate();
-    bounds_control.deactivate();
-    line_control.deactivate();
-    polygon_control.deactivate();
-  }
-}
-
-/*
- * Helper method called on addFeature
- */
-function setItem(feature) {
-  geom = feature.clone().geometry.transform(
-    feature.layer.map.projection,
-    new OpenLayers.Projection('EPSG:4326'));
-    
-  centroid = geom.getCentroid();
-  bounds = geom.getBounds();
-  type = typeLookup(feature);
-
-  feature.layer.map.data_form.wkt.val(geom.toString());
-  feature.layer.map.data_form.type.val(type);
-  feature.layer.map.data_form.lat.val(centroid.y);
-  feature.layer.map.data_form.lon.val(centroid.x);
-  feature.layer.map.data_form.left.val(bounds.left);
-  feature.layer.map.data_form.top.val(bounds.top);
-  feature.layer.map.data_form.bottom.val(bounds.bottom);
-  feature.layer.map.data_form.right.val(bounds.right);
-    
-  for (var i = 0; i < selection_layer.features.length; i++) {
-    if (selection_layer.features[i] != feature) {
-      selection_layer.features[i].destroy();
-    }
-  }
-}
-
-function typeLookup(feature) {
-  lookup = {
-    'OpenLayers.Geometry.Point':'point',
-    'OpenLayers.Geometry.LineString':'line',
-    'OpenLayers.Geometry.Polygon':'polygon',
   };
-  
-  return lookup[feature.geometry.__proto__.CLASS_NAME];
-}
 })(jQuery);
