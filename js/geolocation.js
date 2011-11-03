aa1d9fee8204e185665463bcb60889b5d71888df
nod_ <nod_@598310.no-reply.drupal.org>
2011-11-03 08:14:55 -0400

    [#1274238] - Fixed broken html5 geolocation

--- js/geolocation.js
+++ js/geolocation.js
@@ -1,20 +1,61 @@
-(function ($) {
-  // @TODO: Make this a proper drupal behavior.
-  $(document).ready(function(){
-    if (geo_position_js.init()) {
-    geo_position_js.getCurrentPosition(success_callback, error_callback);
-    }
-    else {
+// geo-location shim
+// Source: https://gist.github.com/366184
+
+// currentely only serves lat/long
+// depends on jQuery
+
+;(function(geolocation, $){
+
+  if (geolocation) return;
+  
+  var cache;
+  
+  geolocation = window.navigator.geolocation = {};
+  geolocation.getCurrentPosition = function(callback){
     
-    }
+    if (cache) callback(cache);
     
-    function success_callback(p) {
-      $('#geofield_lat').val(p.coords.latitude);
-      $('#geofield_lon').val(p.coords.longitude);
-    }
+    $.getScript('//www.google.com/jsapi',function(){
+      
+      cache = {
+        coords : {
+          "latitude": google.loader.ClientLocation.latitude, 
+          "longitude": google.loader.ClientLocation.longitude
+        }
+      };
+      
+      callback(cache);
+    });
     
-    function error_callback(p) {
-    
+  };
+  
+  geolocation.watchPosition = geolocation.getCurrentPosition;
+
+})(navigator.geolocation, jQuery);
+
+;(function ($) {
+  Drupal.behaviors.geofieldGeolocation = {
+    attach: function (context, settings) {
+      // callback for getCurrentPosition
+      function updateLocation(position) {
+        // @TODO: calculate bounding box from accuracy value (accuracy is in meters)
+        $fields.find('.geofield_lat').val(position.coords.latitude);
+        $fields.find('.geofield_lon').val(position.coords.longitude);
+      }
+      
+      // don't do anything if we're on field configuration
+      if (!$(context).find("#edit-instance").length) {
+        var $fields = $(context).find('.field-widget-geofield-geolocation');
+        // check that we have something to fill up
+        // on muti values check only that the first one is empty
+        if ($fields.find('.geofield_lat').val() == '' && $fields.find('.geofield_lon').val() == '') {
+          // very simple geolocation, no fallback support
+          if (navigator.geolocation) {        
+	          navigator.geolocation.getCurrentPosition(updateLocation);
+          }
+        }
+      }
+      
     }
-  });
+  };
 })(jQuery);
