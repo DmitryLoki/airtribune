47ebfc1d772e6608aa737fe1acf040f1b267ea5d
Svad <svadglo@gmail.com>
2013-02-13 21:26:31 +0400

    Issue #2708 Further improvements

--- sites/all/modules/custom/at_entityreference_geowidget/at_entityreference_geowidget.js
+++ sites/all/modules/custom/at_entityreference_geowidget/at_entityreference_geowidget.js
@@ -1,21 +1,21 @@
 (function ($) {
     Drupal.behaviors.atEntityreferenceGeowidget = {
         attach:function (context, settings) {
-            var div, map;
+            var div, map, that = Drupal.behaviors.atEntityreferenceGeowidget;
             div = $('#' + settings.atEntityreferenceGeowidget.mapId + ':not(.at-entityreference-geowidget-processed)', context);
             map = div.data('openlayers');
             if (map !== undefined) {
 
-                var selectControl = map.openlayers.getControlsByClass('OpenLayers.Control.SelectFeature')[0],
+                var selectControl = this.selectControl = map.openlayers.getControlsByClass('OpenLayers.Control.SelectFeature')[0],
                     oldOnSelect = selectControl.onSelect;
 
                 selectControl.onSelect = function (feature) {
-                    if (this.selectedFeature && this.selectedFeature.popup) {
-                        this.selectedFeature.popup.destroy();
-                        this.selectedFeature.popup = null;
+                    if (that.selectedFeature && that.selectedFeature.popup) {
+                        that.selectedFeature.popup.destroy();
+                        that.selectedFeature.popup = null;
                     }
-                    this.selectedFeature = feature;
-                    oldOnSelect.call(this, feature);
+                    that.selectedFeature = feature;
+                    oldOnSelect.call(that, feature);
                     $("#popup a").click(function (event) {
                         event.preventDefault();
                         alert('a')
@@ -29,21 +29,10 @@
                         }
                     })
                 };
-
-                selectControl.onUnselect = function (feature) {
-                    if (this.selectedFeature && this.selectedFeature.popup) {
-                        this.selectedFeature.popup.destroy();
-                        this.selectedFeature.popup = null;
-                    }
-                };
-
-                map.openlayers.events.on({'moveend':Drupal.atEntityreferenceGeowidget.moveEndListener});
+                map.openlayers.events.on({'moveend':that.moveEndListener});
                 div.addClass('at-entityreference-geowidget-processed');
             }
-        }
-    }
-
-    Drupal.atEntityreferenceGeowidget = {
+        },
         moveEndListener:function (event) {
             var request = event.object.getExtent().transform(new OpenLayers.Projection('EPSG:900913'), new OpenLayers.Projection('EPSG:4326')).toString();
             $.get(Drupal.settings.basePath + 'at_entityreference_geowidget', {bbox:request}, $.proxy(Drupal.atEntityreferenceGeowidget.parseResponse, event.object));
@@ -51,30 +40,31 @@
 
         parseResponse:function (response) {
             var layer = this.layers[4],
-                features = [];
-            var wktFormat = new OpenLayers.Format.WKT();
-            //add just those features, that not on a map already
-            for (var i = 0; i < response.length; ++i) {
-                var f = wktFormat.read(response[i].wkt);
-                if (response[i].attribute) {
-                    f.attribute = response[i].attribute;
-                }
-                for (var j = 0; j < layer.features.length; ++j) {
-                    if (!layer.features[j].centroid) {
-                        layer.features[j].centroid = layer.features[j].geometry.getCentroid();
-                    }
-                    var geomCentroid = f.geometry.getCentroid();
-                    if (layer.features[j].centroid.x == geomCentroid.x && layer.features[j].centroid.y == geomCentroid.y) {
+                map = layer.map,
+                that = Drupal.behaviors.atEntityreferenceGeowidget;
+
+            if (map.popups.length) {
+                map.removePopup(map.popups[0]);
+            }
+
+            layer.removeAllFeatures();
+            Drupal.openlayers.addFeatures(Drupal.settings.openlayers.maps[Drupal.settings.atEntityreferenceGeowidget.mapId], layer, response);
+
+            if (that.selectedFeature) {
+                //Select feature that replaces already selected feature
+                var centroid = that.selectedFeature.geometry.getCentroid();
+                for (var i = 0, features = layer.features; i < features.length; ++i) {
+                    var featureCentroid = features[i].geometry.getCentroid();
+                    if (centroid.x == featureCentroid.x && centroid.y == featureCentroid.y) {
+                        if (map.popups.length) {
+                            features[i].popup = window.popup;
+                        }
+                        that.selectControl.onSelect(features[i]);
                         return;
                     }
                 }
-                features.push(f);
             }
-            //Drupal.openlayers.addFeatures(Drupal.settings.openlayers.maps[Drupal.settings.atEntityreferenceGeowidget.mapId], layer, response);
-            layer.addFeatures(features);
-            layer.setVisibility(true);
         },
-
         addReference:function (event) {
             alert('clicked');
         }
