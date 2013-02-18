0dfa4b1d86b699b29c6348775f27165116789a06
Svad <svadglo@gmail.com>
2013-02-18 19:13:27 +0400

    Issue #2623  Clientside validation happens before ajax

--- sites/all/modules/custom/at_validation/js/custom-ajax-form-validation.js
+++ sites/all/modules/custom/at_validation/js/custom-ajax-form-validation.js
@@ -5,7 +5,7 @@
 
         formValidator.errorsFor(this[0]).remove();
 
-        if(errorText){
+        if (errorText) {
             formValidator.showLabel(this[0], errorText);
         }
     }
@@ -16,5 +16,28 @@ Drupal.behaviors.customFormValidation = {
         Drupal.clientsideValidation.prototype.customErrorPlacement = function (error, element) {
             element.after(error);
         };
+
+        var activeField;
+        for(var field in Drupal.settings.ajax) {
+            var f = jQuery(Drupal.settings.ajax[field].selector),
+                event = Drupal.settings.ajax[field].event;
+            //insert event handler before other handlers
+            f.bind(event, function(){
+                activeField = this;
+            });
+            var currentBindings = f.data('events')[event];
+            currentBindings.unshift(currentBindings.pop());
+        }
+
+        var beforeSerialize = Drupal.ajax.prototype.beforeSerialize;
+
+        Drupal.ajax.prototype.beforeSerialize = function (element, options) {
+            if (options.url = '/at-validation/ajax') {
+                var formValidator = element.validate();
+                return formValidator.element(activeField);
+            } else {
+                beforeSerialize.call(this, element, options);
+            }
+        }
     }
 };
