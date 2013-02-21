ab522be76115725948319eedd7fd4bea01b1e093
Svad <svadglo@gmail.com>
2013-02-21 18:29:53 +0400

    Issue #2623 Major changes

--- sites/all/modules/custom/at_validation/js/custom-ajax-form-validation.js
+++ sites/all/modules/custom/at_validation/js/custom-ajax-form-validation.js
@@ -1,62 +1,81 @@
 (function ($) {
-    $.fn.checkValidationResult = function (errorText) {
-        var form = this.parents('form'),
-            formValidator = form.validate();
-        Drupal.clientsideValidation.updateValidationSettings(form);
-        formValidator.errorsFor(this[0]).remove();
-
-        if (errorText) {
-            formValidator.showLabel(this[0], errorText);
-        } else {
-            formValidator.settings.success(this);
-        }
-    }
-})(jQuery);
-
-jQuery(document).ready(function () {
-
-    Drupal.clientsideValidation.updateValidationSettings = function(form){
-        form.validate().settings.errorElement = 'span';
-        form.validate().settings.success = function(element){
-            element.parents('div.form-item').removeClass('field_error');
-        };
-    };
-
-    Drupal.clientsideValidation.prototype.customErrorPlacement = function (error, element) {
-        if(!error.text()){
-            return;
-        }
-        var errorBubble =
-                jQuery('<span class="validate-error form_booble error"><span class="form_booble_inner">' + error.text() + '</span></span>')
-                    .attr('for', element.attr('id'))
-                    .attr('link', element.attr('id')),
-            form = element.parents('div.form-item').addClass('field_error');
-        form.find('span.form_booble.validate-error').remove();
-        element.after(errorBubble);
-    };
 
     var activeField;
-    for (var field in Drupal.settings.ajax) {
-        var f = jQuery(Drupal.settings.ajax[field].selector),
-            event = Drupal.settings.ajax[field].event;
-        //insert event handler before other handlers
-        f.bind(event, function () {
-            activeField = this;
-        });
-        var currentBindings = f.data('events')[event];
-        currentBindings.unshift(currentBindings.pop());
-    }
+    $.fn.checkValidationResult = function (errorText) {
+        var that = this.length ? this : $(activeField),
+            form = that.parents('form'),
+            formValidator = form.validate();
 
-    var beforeSerialize = Drupal.ajax.prototype.beforeSerialize;
-
-    Drupal.ajax.prototype.beforeSerialize = function (element, options) {
-        if (options.url = '/at-validation/ajax') {
-            Drupal.clientsideValidation.updateValidationSettings(element);
-            var formValidator = element.validate();
-            return formValidator.element(activeField);
+        formValidator.errorsFor(that[0]).remove();
+        Drupal.settings.clientsideValidation.updateValidationSettings(formValidator);
+        if (errorText) {
+            formValidator.showLabel(that[0], errorText);
         } else {
-            beforeSerialize.call(this, element, options);
+            formValidator.settings.success();
         }
-    }
+        Drupal.settings.clientsideValidation.updateValidationSettings(formValidator);
+    };
 
-});
+    jQuery(document).ready(function () {
+        Drupal.settings.clientsideValidation.updateValidationSettings = function (formValidator) {
+            formValidator.settings.success = $.proxy(function (error) {
+                var currentElement = this.currentElements,
+                    errorElement = currentElement.data('error-element');
+                currentElement.parents('div.form-item').removeClass('field_error');
+                if (errorElement) {
+                    errorElement.remove();
+                }
+            }, formValidator);
+        };
+
+        for (var f in Drupal.settings.clientsideValidation.forms) {
+            var form = $('#' + f);
+            Drupal.settings.clientsideValidation.updateValidationSettings(form.validate());
+
+        }
+
+        Drupal.clientsideValidation.prototype.customErrorPlacement = function (error, element) {
+            if (!error.text()) {
+                return;
+            }
+            var errorBubble =
+                    jQuery('<span class="validate-error form_booble error"><span class="form_booble_inner">' + error.text() + '</span></span>')
+                        .attr('for', element.attr('id'))
+                        .attr('link', element.attr('id')),
+                form = element.parents('div.form-item').addClass('field_error');
+            form.find('span.form_booble.validate-error').remove();
+            element.data('error-element', errorBubble);
+            element.after(errorBubble);
+        };
+
+        for (var field in Drupal.settings.ajax) {
+            var f = jQuery(Drupal.settings.ajax[field].selector),
+                event = Drupal.settings.ajax[field].event;
+            //insert event handler before other handlers
+            f.bind(event, function () {
+                activeField = this;
+            });
+            var currentBindings = f.data('events')[event];
+            currentBindings.unshift(currentBindings.pop());
+        }
+
+        var beforeSerialize = Drupal.ajax.prototype.beforeSerialize,
+            ajaxSuccess = Drupal.ajax.prototype.success;
+
+        Drupal.ajax.prototype.beforeSerialize = function (element, options) {
+            if (options.url = '/at-validation/ajax') {
+                var formValidator = element.validate();
+                Drupal.settings.clientsideValidation.updateValidationSettings(formValidator);
+                return formValidator.element(activeField);
+            } else {
+                beforeSerialize.call(this, element, options);
+            }
+        };
+
+        Drupal.ajax.prototype.success = function(){
+            ajaxSuccess.apply(this, arguments);
+            var formValidator = $(activeField).parents('form').validate();
+            Drupal.settings.clientsideValidation.updateValidationSettings(formValidator);
+        }
+    });
+})(jQuery);
