d528b175cf49bce6ae8035bf0ea2f91b51db37b8
Vasily Kraev <mail@vkraev.ru>
2013-03-02 06:33:15 +0400

    #2764 - js empty photo field

--- sites/all/modules/custom/at_user/js/auto_upload.js
+++ sites/all/modules/custom/at_user/js/auto_upload.js
@@ -1,6 +1,13 @@
 (function ($) {
   Drupal.behaviors.autoUpload = {
     attach: function (context, settings) {
+
+      var widget = $('.image-widget').first();
+      if (widget.find('.image-preview').size() == 0) {
+        widget.addClass('photo-empty');
+        widget.append("<div class='photo-empty-div'></div>").addClass();
+      }
+
       $('form', context).delegate('input.form-file', 'change', function() {  
         $(this).next('input[type="submit"]').mousedown();
       }); 
@@ -9,7 +16,8 @@
         if (confirm(Drupal.t('Are you sure you want to delete the your photo?'))) {
           $(this).parent().find('.image-widget-data input[type="submit"]').mousedown();
         }
-      }); 
+      });
+
     }
   };
 })(jQuery);
