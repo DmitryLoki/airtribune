3d764ebfb837f58b37bce168eb391d7ec210084e
Vyacheslav Malchik <info@vkey.biz>
2013-03-06 18:23:18 +0600

    #2764 restored user-profile-form.tpl.php

--- sites/all/themes/airtribune2/templates/user-profile-form.tpl.php
+++ sites/all/themes/airtribune2/templates/user-profile-form.tpl.php
@@ -34,12 +34,41 @@ if (!empty($form['profile_main'])) {
   $form['profile_main']['field_full_name'][$lang][0]['given']['#attributes']['rel'] = t('Enter your name');
   $form['profile_main']['field_full_name'][$lang][0]['family']['#attributes']['rel'] = t('Enter your surname');
 
+  print drupal_render($form['profile_main']['field_full_name'][$lang][0]['given']);
+  print drupal_render($form['profile_main']['field_full_name'][$lang][0]['family']);
+  drupal_render($form['profile_main']['field_full_name']);
+  print drupal_render($form['profile_main']['field_gender']);
+
   $lang = $form['profile_main']['field_birthdate']['#language'];
   $form['profile_main']['field_birthdate'][$lang][0]['value']['month']['#title'] = $form['profile_main']['field_birthdate'][$lang][0]['#title'];
   $form['profile_main']['field_birthdate'][$lang][0]['#title'] = '';
-
+  print drupal_render($form['profile_main']['field_birthdate']);
 }
 
-$form['account']['pass']['#attached']['js'][0] = 'sites/all/themes/airtribune2/js/user.js';
+$form['pass_fieldset'] = array(
+  '#type' => 'fieldset',
+  '#title' => t('Change password'),
+  '#prefix' => '<div id="password-fieldset">',
+  '#suffix' => '</div>',
+  '#collapsible' => TRUE,
+  '#collapsed' => TRUE,
+  '#attributes' => array('class' => array('collapsible', 'collapsed')),
+);
 
+$form['pass_fieldset']['current_pass'] = $form['account']['current_pass'];
+$form['pass_fieldset']['pass']         = $form['account']['pass'];
+print drupal_render($form['pass_fieldset']);
+
+// hide elements
+hide($form['account']['pass']);
+hide($form['account']['current_pass']);
+hide($form['timezone']);
+hide($form['locale']);
+
+hide($form['og_user_node']);
+hide($form['xmlsitemap']);
+hide($form['metatags']);
+
+// Hide wrapping fieldsets.
+drupal_render($form['profile_main']);
 print drupal_render_children($form);
