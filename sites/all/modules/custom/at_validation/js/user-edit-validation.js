408e4f5e0d6ba488bc279fd6ad2476d7d2dacc1e
Svad <svadglo@gmail.com>
2013-03-12 17:49:52 +0400

    Issue #2623 Submit button on edit user page reacts on keyup in pass fields.

--- sites/all/modules/custom/at_validation/js/user-edit-validation.js
+++ sites/all/modules/custom/at_validation/js/user-edit-validation.js
@@ -4,10 +4,19 @@ jQuery(function ($) {
         passFields = $('#edit-current-pass,#edit-pass-pass1,#edit-pass-pass2'),
         formValidator = passwordFieldset.closest('form').validate();
     passwordFieldset.find('a.fieldset-title').bind('mousedown', function () {
-        setTimeout(function() {
-            console.log(passFields.is(':hidden'));
-            Drupal.checkAllElementsValid(formValidator);
-        },500);
+        setTimeout(function () {
+            if (passwordFieldsetWrapper.is(':visible')) {
+                passFields.bind('keyup', function () {
+                    Drupal.checkAllElementsValid(formValidator);
+                    passFields.unbind('keyup', arguments.callee);
+                })
+            } else {
+                passFields.val('');
+                passFields.removeClass('error').siblings('.form_booble').css('display','none');
+                passFields.parent().removeClass('field_error field_good field_excellent');
+                Drupal.checkAllElementsValid(formValidator);
+            }
+        }, 500);
 
     })
 });
