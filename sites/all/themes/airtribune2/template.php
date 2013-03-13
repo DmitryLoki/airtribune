bc6729ef8353afcefb0d198bd72eb5fb7f69f792
Zander <akma_2003@mail.ru>
2013-03-13 21:48:28 +0000

    Issue #2791 comment 10

--- sites/all/themes/airtribune2/template.php
+++ sites/all/themes/airtribune2/template.php
@@ -1265,3 +1265,35 @@ function airtribune2_css_alter(&$css) {
   //unset($css[drupal_get_path('module', 'system') . '/defaults.css']);
   unset($css[drupal_get_path('module', 'date') . '/date_api/date.css']);
 }
+
+/**
+ * Implements theme_status_messages().
+ */
+function airtribune2_status_messages($variables) {
+  $display = $variables['display'];
+  $output = '';
+
+  $status_heading = array(
+    'status' => t('Status message'),
+    'error' => t('Error message'),
+    'warning' => t('Warning message'),
+  );
+  foreach (drupal_get_messages($display) as $type => $messages) {
+    $output .= "<div class=\"messages $type\">\n";
+    if (!empty($status_heading[$type])) {
+      $output .= '<h2 class="element-invisible">' . $status_heading[$type] . "</h2>\n";
+    }
+    if (count($messages) > 1) {
+      $output .= " <ul>\n";
+      foreach ($messages as $message) {
+        $output .= '  <li>' . $message . "</li>\n";
+      }
+      $output .= " </ul>\n";
+    }
+    else {
+      $output .= $messages[0] . '<span class="valign"></span>';
+    }
+    $output .= "</div>\n";
+  }
+  return $output;
+}
