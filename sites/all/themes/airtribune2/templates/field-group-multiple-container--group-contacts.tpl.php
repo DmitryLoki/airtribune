4b5f958394792b96044a3b5bb665f615a3f16e83
Chivse <no-reply@example.com>
2013-03-17 12:16:10 +0600

    Issue #2703: Added new contacts fields to pg_contest content type.

--- sites/all/themes/airtribune2/templates/field-group-multiple-container--group-contacts.tpl.php
+++ sites/all/themes/airtribune2/templates/field-group-multiple-container--group-contacts.tpl.php
@@ -5,39 +5,18 @@
  *  - delta + field_name + value render_array or null if empty
  */
 
-$labels = array(
-  t('General questions'),
-  t('Selection and entry'),
-  t('Transportation'),
-  t('Accomodation')
-);
-
 ?>
 <div class="clearfix">
   <?php foreach($entries as $delta => $entry): ?>
-    <?php
-      $wrapper_class = 'field-wrapper'; 
-      if ($delta%2) {
-        $wrapper_class .= ' even';
-      } 
-    ?>
-    <div class="<?php print $wrapper_class; ?>">
-      <h3><?php print $labels[$delta]; ?></h3>
-      <div class="clearfix">
-      <?php
-        $entry['field_contacts_email']['#title'] = t('email');
-        $entry['field_contacts_phone']['#title'] = t('tel');
-      ?>
-      <?php foreach($entry as $field_name => $field): ?>
-        <div>
-        <?php if(!empty($field[0]['#markup'])): ?>
+  <hr/>
+  <div class="clearfix">
+    <?php foreach($entry as $field_name => $field): ?>
+      <?php if (!empty($field[0]['#markup'])): ?>
+        <div class="field-item-<?php print $field_name; ?>">
           <?php print render($field); ?>
-        <?php else: ?>
-          <span class="field-is-empty"> </span>
-        <?php endif; ?>
         </div>
-      <?php endforeach; ?>
-      </div>
-    </div>
+      <?php endif; ?>
+    <?php endforeach; ?>
+  </div>
   <?php endforeach; ?>
 </div>
