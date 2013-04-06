4f4fe4db2405751b36d6967c6b44e74ba7e2fe0a
Svad <svadglo@gmail.com>
2013-04-06 07:42:43 +0400

    Issue #2846 Height of title after collapsing calculated via js, not set in css.

--- sites/all/themes/airtribune2/js/header-collapser.js
+++ sites/all/themes/airtribune2/js/header-collapser.js
@@ -6,14 +6,24 @@ jQuery(function ($) {
     }
 
     var headerContainer = $('.featured-header'),
+        headerTitle = $('.featured-header-title'),
+        headerContainerHeight = headerContainer.height(),
+        headerTitleHeight = headerTitle.outerHeight() || 100,
         collapsedClass = 'featured-header-collapsed',
         collapseControl = $('<div class="collapse-control">');
-
 
     headerContainer.append(collapseControl);
 
     collapseControl.bind('click', function () {
         headerContainer.toggleClass(collapsedClass);
+        toggleHeight();
     });
-
+    toggleHeight();
+    function toggleHeight() {
+        if(headerContainer.hasClass(collapsedClass)) {
+            headerContainer.height(headerTitleHeight)
+        } else {
+            headerContainer.height(headerContainerHeight);
+        }
+    }
 });
