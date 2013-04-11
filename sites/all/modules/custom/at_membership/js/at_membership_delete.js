ae8389ea612064ce5c3181e3584f1c2a0f72de23
Ivan Ilinov <i.ilinov@gmail.com>
2013-04-11 11:24:11 +0000

    #3003 - Registrations info included on PM with access plugin, fixed title and path, added new user permissions

--- sites/all/modules/custom/at_membership/js/at_membership_delete.js
+++ sites/all/modules/custom/at_membership/js/at_membership_delete.js
@@ -1,34 +1,35 @@
 (function ($) {
   Drupal.behaviors.atMembershipDelete = {
     attach: function(context, settings) {
-      $('.event-signout', context).click(function(){
-        var
-          a = $(this),
-          td = a.parents('td').eq(0),
-          tr = td.parents('tr').eq(0);
-        var
-          deleteSuccess = function(data){
-            alert(data.message);
-            tr.remove();
-          },
-          deleteError = function(data){
-            alert(Drupal.t('Sign out fails.'));
-          },
-          deleteComplete = function(){
-            a.show();
-            $('.event-signout-process', td).remove();
-          }
-        a.hide();
-        td.append('<span class="event-signout-process">' + Drupal.t('Process...') + '</span>');
-        $.ajax({
-          type: 'POST',
-          url: this.href,
-          dataType: 'json',
-          success: deleteSuccess,
-          error: deleteError,
-          complete: deleteComplete,
-          data: 'js=1'
-        });
+      $('.cancel-participation', context).click(function(){
+        if (confirm(Drupal.t('Are you sure to cancel your participation on this event?'))) {
+          var
+            a = $(this),
+            row = a.parents('.views-row').eq(0);
+          var
+            deleteSuccess = function(data){
+              alert(data.message);
+              row.slideUp('fast', function () { $(this).remove(); });
+            },
+            deleteError = function(data){
+              alert(Drupal.t('Cancel your participation fails.'));
+            },
+            deleteComplete = function(){
+              a.show();
+              $('.cancel-participation-process', row).remove();
+            }
+          a.hide();
+          row.append('<span class="cancel-participation-process">' + Drupal.t('Process...') + '</span>');
+          $.ajax({
+            type: 'POST',
+            url: this.href,
+            dataType: 'json',
+            success: deleteSuccess,
+            error: deleteError,
+            complete: deleteComplete,
+            data: 'js=1'
+          });
+        }
         return false;
       });
     }
