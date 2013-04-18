49617b32b17062a68deba67b21532923a44339b8
Chivse <no-reply@example.com>
2013-04-19 03:22:04 +0600

    Issue 3025: Added day ref field to event days view.

--- sites/all/themes/airtribune2/templates/views-view-fields--event-days.tpl.php
+++ sites/all/themes/airtribune2/templates/views-view-fields--event-days.tpl.php
@@ -4,12 +4,11 @@
  * @file
  * Event days fields.
  */
-
 ?>
 <h3>
 <?php print $fields['title']->content; ?> — <?php print $fields['field_day_status']->content; ?>
-<?php if ($fields['field_pg_race_ref']->content): ?>
-  — <?php print $fields['field_pg_race_ref']->content; ?>
+<?php if ($fields['title_1']->content): ?>
+  — <?php print $fields['title_1']->content; ?>
 <span><?php print $fields['created']->content; ?></span>
 <?php endif; ?>
 </h3>
