a41ab762c593ea5a12b5a1207303b420b6c8f99e
Zander <akma_2003@mail.ru>
2013-04-18 21:56:47 +0000

    Issue #3058

--- sites/all/themes/airtribune2/templates/views-view-fields--event-days.tpl.php
+++ sites/all/themes/airtribune2/templates/views-view-fields--event-days.tpl.php
@@ -4,14 +4,76 @@
  * @file
  * Event days fields.
  */
+$photos = array();
+if($row->field_field_photos){
+    $count = count($row->field_field_photos);
+  switch($count){
+    case 1:
+      $image_styles = array('event_blog_teaser_first');
+    break;
+    case 2:
+      $image_styles = array('event_blog_teaser_second');
+    break;
+    case 3:
+      $image_styles = array('event_blog_teaser_third', 'event_blog_teaser_fourth');
+    break;
+    case 4:
+      $image_styles = array('event_blog_teaser_fifth', 'event_blog_teaser_sixth');
+    break;
+    case 5:
+      $image_styles = array('event_blog_teaser_first', 'event_blog_teaser_sixth_extra', 'event_blog_teaser_sixth_extra', 'event_blog_teaser_sixth');
+    break;
+    case 6:
+      $image_styles = array('event_blog_teaser_second', 'event_blog_teaser_second', 'event_blog_teaser_sixth_extra', 'event_blog_teaser_sixth', 'event_blog_teaser_sixth_extra', 'event_blog_teaser_sixth');
+    break;
+    case 7:
+      $image_styles = array('event_blog_teaser_first', 'event_blog_teaser_seventh');
+    break;
+    case 8:
+      $image_styles = array('event_blog_teaser_second', 'event_blog_teaser_second', 'event_blog_teaser_seventh');
+    break;
+    case 9:
+      $image_styles = array('event_blog_teaser_third', 'event_blog_teaser_fourth', 'event_blog_teaser_fourth', 'event_blog_teaser_seventh');
+    break;
+    default:
+      $image_styles = array('event_blog_teaser_first', 'event_blog_teaser_eighth');
+    break;
+  }
+  $is_count = 0;
+  foreach($row->field_field_photos as $k => $v){
+    if(!empty($image_styles[$is_count])){
+      $image_style = $image_style_other = $image_styles[$is_count];
+    }
+    else{
+      $image_style = $image_style_other;
+    }
+    if($v['rendered']['#theme'] != 'colorbox_image_formatter'){
+      $v['rendered']['#image_style'] = $image_style;
+    }
+    else{
+      $v['rendered']['#display_settings']['colorbox_node_style'] = $image_style;
+    }
+    $photos[] = $v['rendered'];
+    $is_count++;
+    
+  }
+
+}
 
 ?>
 <h3>
+<?php if ($fields['field_pg_race_ref']->content): ?>
+<span><?php print $fields['created']->content; ?></span>
+<?php endif; ?>
 <?php print $fields['title']->content; ?> — <?php print $fields['field_day_status']->content; ?>
 <?php if ($fields['field_pg_race_ref']->content): ?>
   — <?php print $fields['field_pg_race_ref']->content; ?>
-<span><?php print $fields['created']->content; ?></span>
 <?php endif; ?>
 </h3>
-<?php print $fields['field_day_blog']->content; ?>
-<?php print $fields['field_photos']->content; ?>
+<div class="day-content">
+  <?php print $fields['field_day_blog']->content; ?>
+
+  <?php print render($photos); ?>
+</div>
+
+
