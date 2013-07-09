e767a05cbc26e2a1d9ef3d7a2ec7a65cd49b6ce4
Vyacheslav Malchik <info@vkey.biz>
2013-07-09 11:10:43 +0000

    restore scripts on player page

--- sites/all/themes/airtribune2/templates/html--play.tpl.php
+++ sites/all/themes/airtribune2/templates/html--play.tpl.php
@@ -58,10 +58,13 @@
 <!--[if IE 8]><html class="lt-ie9"<?php print $html_attributes; ?>><![endif]-->
 <!--[if (gte IE 9)|(gt IEMobile 7)]><!--><html<?php print $html_attributes . $rdf_namespaces; ?>><!--<![endif]-->
 <head>
-<?php print $head; ?>
-<title><?php print $head_title; ?></title>
+  <?php print $head; ?>
+  <title><?php print $head_title; ?></title>
+  <?php print $scripts; ?>
 </head>
 <body class="<?php print $classes; ?>"<?php print $attributes; ?>>
+  <?php print $page_top; ?>
   <?php print $page; ?>
+  <?php print $page_bottom; ?>
 </body>
 </html>
