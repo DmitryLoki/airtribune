f1b7140385c55b42caceb4efa42bb7f2a001cef4
Svad <svadglo@gmail.com>
2013-08-26 07:37:05 +0400

    #3543 Change location in soon in country, js slider

--- sites/all/modules/custom/airtribune/plugins/content_types/soon_country/soon_country.js
+++ sites/all/modules/custom/airtribune/plugins/content_types/soon_country/soon_country.js
@@ -1,11 +1,31 @@
 jQuery(function ($) {
-  $('.countries_title_suffix_inner').each(function () {
-    $(this).parent().show();
-    $(this).jScrollPane({scrollbarWidth:4, showArrows:false});
-    $(this).parent().hide(); 
 
+  $('.countries_title_suffix_inner').each(function () {
+
+    var countriesInner = $(this);
+    countriesInner.parent().show();
+    countriesInner.jScrollPane({scrollbarWidth:4, showArrows:false});
+    //fix jsp wrong height calculation
+    countriesInner.css('padding-top','10px');
+    var countriesTitleHeight = $(this).outerHeight(),
+      countriesTitleSuffix = $('.countries_title_suffix');
+
+    $('.change_location_title').click(function(e) {
+      e.stopImmediatePropagation();
+      //toggle height
+      if(countriesTitleSuffix.height() > 0) {
+        countriesTitleSuffix.height(0);
+        $(document.body).unbind('click',bodyClickHandler);
+      } else {
+        countriesTitleSuffix.height(countriesTitleHeight);
+        $(document.body).bind('click',bodyClickHandler);
+      }
+    });
+
+    function bodyClickHandler(e) {
+      if(countriesInner.find(e.target).length == 0) {
+        countriesTitleSuffix.height(0);
+      }
+    }
   });
-  $('.change_location_title').click(function(){
-    $('.countries_title_suffix').show().height($('.countries_title_suffix_inner').outerHeight());
-  })
 });
