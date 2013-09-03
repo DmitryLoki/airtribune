a6687b4e9cca6f11f2042e3583aec1055e0111f3
Svad <svadglo@gmail.com>
2013-09-03 16:19:28 +0400

    #3541 #3543 Big flag in "Soon in country" block after country selection

--- sites/all/themes/airtribune2/js/frontpage.js
+++ sites/all/themes/airtribune2/js/frontpage.js
@@ -1,19 +1,30 @@
-jQuery(function($){
-  $('section.front_live_events').each(function(i,section){
-    var $section = $(section),
-      clickCounter = 1,
-      rowsContainer = $section.find('.view-frontpage-events > .view-content'),
-      rowsContainerHeight = rowsContainer.height(),
-      expandButton = $section.find('.frontpage-show-more-events'),
-      viewAllEventsButton = $section.find('h2.pane-title a');
+(function($){
+  Drupal.behaviors.frontpage = {
+    attach: function(context){
+      $('section.front_live_events').each(function(i,section){
+        var $section = $(section),
+          clickCounter = 1,
+          itemsPerRow = 3,
+          maxRowsCount = 3,
+          items = $section.find('.views-row'),
+          rowsCount = Math.ceil(items.length / itemsPerRow),
+          rowsContainer = $section.find('.view-frontpage-events > .view-content'),
+          rowsContainerHeight = rowsContainer.height(),
+          expandButton = $section.find('.frontpage-show-more-events'),
+          viewAllEventsButton = $section.find('h2.pane-title a');
 
-    expandButton.bind('click', function() {
-      if(clickCounter == 3) {
-        location.href = viewAllEventsButton.attr('href');
-        return;
-      }
-      rowsContainer.height(rowsContainerHeight * ++clickCounter);
+        expandButton.bind('click', function() {
+          if(clickCounter == rowsCount && viewAllEventsButton.attr('href')) {
+            location.href = viewAllEventsButton.attr('href');
+            return;
+          }
+          if(clickCounter < maxRowsCount) {
+            rowsContainer.height(rowsContainerHeight * ++clickCounter);
+          }
 
-    });
-  })
-});
+        });
+      })
+    }
+  }
+
+})(jQuery);
