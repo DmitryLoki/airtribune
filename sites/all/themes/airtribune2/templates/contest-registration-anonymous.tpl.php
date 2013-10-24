7cd4f142ad0bbbba963a42460cedc5d9e48f33ed
Zander <akma_2003@mail.ru>
2013-10-25 00:27:21 +0100

    Issue #3638 comment 9

--- sites/all/themes/airtribune2/templates/contest-registration-anonymous.tpl.php
+++ sites/all/themes/airtribune2/templates/contest-registration-anonymous.tpl.php
@@ -2,5 +2,6 @@
 
 // Hide OG registration Cancel link
 drupal_render($form['multiform'][1]['actions']['cancel']);
+$form['multiform'][3]['#attributes']['class'][] = 'form-actions';
 
 print drupal_render_children($form);
--- sites/all/themes/airtribune2/templates/contest-registration-authorized.tpl.php
+++ sites/all/themes/airtribune2/templates/contest-registration-authorized.tpl.php
@@ -2,5 +2,6 @@
 
 // Hide OG registration Cancel link
 drupal_render($form['multiform'][0]['actions']['cancel']);
+$form['multiform'][2]['#attributes']['class'][] = 'form-actions';
 
 print drupal_render_children($form);
