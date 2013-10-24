8c4dff3852dcd2deafc4b974f767b41b7dda86fd
Alexandr Kapelshikov <noreply@example.com>
2013-10-25 00:40:09 +0100

    ref #3638 - minor fix

--- sites/all/themes/airtribune2/templates/contest-registration-authorized.tpl.php
+++ sites/all/themes/airtribune2/templates/contest-registration-authorized.tpl.php
@@ -2,6 +2,10 @@
 
 // Hide OG registration Cancel link
 drupal_render($form['multiform'][0]['actions']['cancel']);
-$form['multiform'][2]['#attributes']['class'][] = 'form-actions';
+
+drupal_render($form['multiform'][1]['actions']);
+
+$form['multiform'][2]['register']['#prefix'] = '<div class="form-actions">';
+$form['multiform'][2]['register']['#suffix'] = '</div>';
 
 print drupal_render_children($form);
