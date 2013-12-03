e0c5fc7398858b69ab9aee17dbae1dfb7753d546
Khegai Sergei <nodoxi@gmail.com>
2013-12-03 11:47:07 +0000

    Issue #4026 fix filter input

--- sites/all/modules/custom/airtribune/js/pilots-filter-table.js
+++ sites/all/modules/custom/airtribune/js/pilots-filter-table.js
@@ -1,5 +1,8 @@
 jQuery(function ($) {
-  $(".view-paragliding-pilots-list table").filterTable();
+  $(".view-paragliding-pilots-list table").filterTable({
+    minRows: 0,
+    label: ""
+  });
   var $main_input = $(".filter-table:first input");
   $main_input.keyup(function(){
     $(".filter-table input").not(this).val(jQuery(this).val()).keyup();
