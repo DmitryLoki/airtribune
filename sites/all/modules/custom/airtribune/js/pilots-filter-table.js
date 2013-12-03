f81844f87de124d573bb1f207e5333dfae7a50e0
Khegai Sergei <nodoxi@gmail.com>
2013-12-03 11:37:44 +0000

    Issue #4026 one filter on two tables

--- sites/all/modules/custom/airtribune/js/pilots-filter-table.js
+++ sites/all/modules/custom/airtribune/js/pilots-filter-table.js
@@ -1,3 +1,8 @@
 jQuery(function ($) {
   $(".view-paragliding-pilots-list table").filterTable();
+  var $main_input = $(".filter-table:first input");
+  $main_input.keyup(function(){
+    $(".filter-table input").not(this).val(jQuery(this).val()).keyup();
+  });
+  $(".filter-table input").not($main_input).hide();
 });
