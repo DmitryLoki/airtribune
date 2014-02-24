8e6da4f07d89e1c44576a16fbf342926b9a81ae0
Vyacheslav Malchik <info@vkey.biz>
2014-02-24 11:43:52 +0000

    #4422 add tracker field module

---
+++ sites/all/modules/custom/at_tracker/js/at_tracker.js
@@ -0,0 +1,19 @@
+(function ($) {
+
+  Drupal.behaviors.at_tracker = {
+    attach: function (context, settings) {
+      $.fn.changeBindTrackerField = function(data) {
+        var field = $('select[name="' + data + '[und][0][value]"]'),
+        fieldAjax = Drupal.ajax[field.attr('id')];
+        field.unbind('mousedown');
+        field.on('click', function(){
+          field.attr('size',1);
+          field.on('mousedown', function(event){
+            fieldAjax.eventResponse(field, event);
+          });
+        });
+      }
+    }
+  }
+
+})(jQuery);
