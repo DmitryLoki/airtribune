996df63e76b5466757bdf7c860739f459beac6ea
Vyacheslav Malchik <info@vkey.biz>
2014-02-26 12:37:24 +0000

    #4422 fix options list cache

--- sites/all/modules/custom/at_tracker/js/at_tracker.js
+++ sites/all/modules/custom/at_tracker/js/at_tracker.js
@@ -2,8 +2,8 @@
 
   Drupal.behaviors.at_tracker = {
     attach: function (context, settings) {
-      $.fn.changeBindTrackerField = function(data) {
-        var field = $('select[name="' + data + '[und][0][value]"]'),
+      $.fn.changeBindTrackerField = function(data, wrapperId) {
+        var field = $(wrapperId + ' select[name="' + data + '[und][0][value]"]'),
         fieldAjax = Drupal.ajax[field.attr('id')];
         field.unbind('mousedown');
         field.on('click', function(){
