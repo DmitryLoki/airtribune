06f57b4de4a2d63c6fd9c88abe18552d47be70ea
Vasily Kraev <mail@vkraev.ru>
2014-03-26 21:27:36 +0600

    #4498 - use dump-field 'optional_url' instead common field tiny_path_url, to avoid problem with reverting field with pg_contest.

--- sites/all/themes/airtribune2/templates/user-profile-form.tpl.php
+++ sites/all/themes/airtribune2/templates/user-profile-form.tpl.php
@@ -84,11 +84,11 @@ $form['field_header_image']['und'][0]['select']['#title'] = t('Add image');
 $form['field_header_image']['und'][0]['remove']['#title'] = t('Delete');
 
 $form['personalisation']['basic_url'] = $form['basic_url'];
-$form['personalisation']['field_tiny_path'] = $form['field_tiny_path'];
+$form['personalisation']['optional_url'] = $form['optional_url'];
 $form['personalisation']['field_header_image'] = $form['field_header_image'];
 $form['personalisation']['field_social_links'] = $form['field_social_links'];
 unset($form['basic_url']);
-unset($form['field_tiny_path']);
+unset($form['optional_url']);
 unset($form['field_header_image']);
 unset($form['field_social_links']);
 
